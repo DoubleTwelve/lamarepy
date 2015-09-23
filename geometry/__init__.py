c7e021d2a8197c2868c0ae5c88634967014bbe26
DoubleTwelve <muratov.oleg@gmail.com>
2015-09-23 17:18:49 +0300

    [ADD] dummy quaternion

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,1 +1,3 @@
 import numpy as np
+from quaternion import quaternion
+import convertors
--- geometry/quaternion.py
+++ geometry/quaternion.py
@@ -1,13 +1,16 @@
-class Quaternion:
-    __coeffs = np.array([0., 0., 0., 1.])
+from ..geometry import np
 
+class quaternion:
+
+    def __init__(self, coeffs=[0., 0., 0., 1.]):
+        self._coeffs = np.array([coeffs])
 
     def vec(self):
-        return self.__coeffs[0:3]
+        return self._coeffs[0:3]
 
     def coeffs(self):
-        return self.__coeffs
+        return self._coeffs
 
     def normalize(self):
-        norm = np.linalg.norm(self.__coeffs)
-        self.__coeffs = self.__coeffs/norm
+        norm = np.linalg.norm(self._coeffs)
+        self._coeffs = self._coeffs/norm
